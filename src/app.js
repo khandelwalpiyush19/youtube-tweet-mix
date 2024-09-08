8cd81f5774a6cab7971ac2f38a8cff608dffd979
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2024-09-08 16:54:50 +0530

    progress of project

--- src/app.js
+++ src/app.js
@@ -13,6 +13,26 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true ,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+app.use("/api/v1/users",userRouter)
+
+
+
+
+
+
+
+
+
+
+
+
 export {app}
 
 
